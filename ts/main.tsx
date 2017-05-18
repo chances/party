a6543b17b76c76611ad2136eb1ec8cee3c950d81
Chance Snow <git@chancesnow.me>
2017-05-18 02:17:39 -0700

    Render main component in main element

--- ts/main.tsx
+++ ts/main.tsx
@@ -3,7 +3,14 @@ import * as util from './util'
 
 let foo = util.log('Foo:', { jank: 'free' })
 
-render(
-  <p>Foobar</p>,
-  document.body
-)
+let main = document.querySelector('main')
+if (main !== null) {
+  main.remove()
+  render(
+    <main>
+      <h2>chancesnow.me/party</h2>
+      <p>Foobar</p>
+    </main>,
+    document.body
+  )
+}
