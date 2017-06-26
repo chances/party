4f7265394f29b9b5f93d046f2f7d138a2f5b625d
Chance Snow <git@chancesnow.me>
2017-06-26 04:14:03 -0700

    Make join form more robust

--- ts/components/join-form.tsx
+++ ts/components/join-form.tsx
@@ -9,11 +9,26 @@ interface Props {
   onJoinSubmitted(partyCode: string): void
 }
 
+const whitespace = /[\s\^]/g
+const alphaNum = /[a-zA-z0-9]/
+function stripNonAlphaNumeric(s: string) {
+  let result = ''
+  for (let char = 0; char < s.length; char++) {
+    const c = s.charAt(char)
+    result += alphaNum.test(c) ? c : ''
+  }
+  return result
+}
+
 export default class JoinForm extends Component<Props, {}> {
   private partyCode: HTMLInputElement
+  private submitButton: HTMLInputElement
 
   render({partyCode, joining}: Props, {}) {
     const spinnerHidden = !(joining != null && joining)
+    const submitHidingClass = spinnerHidden && partyCode &&
+      partyCode.trim() ? '' : 'hiding'
+
     return (
       <form id="join" onSubmit={this.onJoinSubmitted}>
         <h1>Join a Party</h1>
@@ -30,10 +45,14 @@ export default class JoinForm extends Component<Props, {}> {
               autofocus={true}
               autocomplete="off"
               maxLength={4}
+              disabled={!spinnerHidden}
+              onKeyUp={this.partyCodeKey}
               onFocus={this.focusBlurJoinForm}
               onBlur={this.focusBlurJoinForm}
             />
             <input
+              ref={this.refSubmitButton}
+              class="hiding"
               type="submit"
               value="Join"
               onFocus={this.focusBlurJoinForm}
@@ -50,6 +69,30 @@ export default class JoinForm extends Component<Props, {}> {
     this.partyCode = node
   }
 
+  private refSubmitButton = (node: HTMLInputElement) => {
+    this.submitButton = node
+  }
+
+  private partyCodeKey = (e: Event) => {
+    const input = e.currentTarget as HTMLInputElement
+    const value = stripNonAlphaNumeric(
+      input.value.trim().replace(whitespace, ''),
+    )
+    input.value = value
+
+    if (!this.submitButton) {
+      return
+    }
+
+    const showSubmit = value.length > 0
+
+    if (showSubmit) {
+      this.submitButton.classList.remove('hiding')
+    } else if (!this.submitButton.classList.contains('hiding')) {
+      this.submitButton.classList.add('hiding')
+    }
+  }
+
   private focusBlurJoinForm(e: Event) {
     const input = e.currentTarget as HTMLInputElement
     const pillGroup = input.parentElement as HTMLElement
@@ -64,7 +107,13 @@ export default class JoinForm extends Component<Props, {}> {
   private onJoinSubmitted = (e: Event) => {
     e.preventDefault()
 
-    if (this.partyCode && this.partyCode.value) {
+    if (this.partyCode && this.partyCode.value && this.partyCode.value.trim()) {
+      if (!this.submitButton.classList.contains('hiding')) {
+        this.submitButton.classList.add('hiding')
+      }
+      this.partyCode.blur()
+      this.submitButton.blur()
+
       this.props.onJoinSubmitted(this.partyCode.value)
     }
   }
