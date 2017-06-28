d0951e62228438f2ea8145639588d5b17cc69bdb
Chance Snow <git@chancesnow.me>
2017-06-28 03:26:32 -0700

    Make api request utils more robust, support GET and POST

--- ts/api/request.ts
+++ ts/api/request.ts
@@ -13,28 +13,68 @@ const defaultOptions: RequestInit = {
 }
 
 export interface RequestParam {name: string, value: string}
-export type Response<T> = Either<any, T>
+export type Response<T> = Either<Errors, T>
 export type ResponsePromise<T> = Promise<Response<T>>
 export enum RequestStatus {
   LOADING,
   COMPLETED,
 }
 
+export interface Error {
+  status: number
+  title: string
+  detail: string
+  meta: {
+    cause: string | null,
+    details: string | null,
+  }
+}
+export interface Errors {
+  responseStatus: number | null
+  errors: Error[]
+}
+
 export function get<T>(path: string, params?: RequestParam[]): ResponsePromise<T> {
+  return request<T>('get', path, undefined, params)
+}
+
+export function post<T>(path: string, body?: {data: any},
+                        params?: RequestParam[]): ResponsePromise<T> {
+  return request<T>('post', path, body, params)
+}
+
+function request<T>(method: 'get' | 'post', path: string, body?: {data: any},
+                    params?: RequestParam[]): ResponsePromise<T> {
   const url = partyApiHost + path +
     (params ? joinParams(params) : '')
+  const headers = body != null
+    ? {'Content-Type': 'application/json'}
+    : undefined
+  const options: RequestInit = {
+    ...defaultOptions,
+    method,
+    headers,
+    body: body ? JSON.stringify(body) : undefined,
+  }
 
   return new Promise(resolve => {
-    fetch(new Request(url, defaultOptions)).then(response => {
-      if (!response.ok) {
-        response.json().then(data => {
-          resolve(Either.Left('error') as Either<any, T>)
-        })
-      }
-
+    fetch(new Request(url, options)).then(response => {
       response.json().then(data => {
-        resolve(Either.Right(data))
+        const result = response.ok
+          ? Either.Right(data.data)
+          : Either.Left<Errors>({
+            responseStatus: response.status,
+            ...data,
+          })
+        resolve(result)
       })
+    }).catch(e => {
+      resolve(Either.Left<Errors>({
+        responseStatus: null,
+        errors: [
+          createError('Request Error', 'Party could not complete the request'),
+        ],
+      }))
     })
   }) as ResponsePromise<T>
 }
@@ -49,4 +89,16 @@ function joinParams(params: RequestParam[]): string {
   return params.map(param => {
     return encodeURIComponent(param.name) + '=' + encodeURIComponent(param.value)
   }).join('&')
+}
+
+function createError(title: string, detail: string, cause?: string, details?: string): Error {
+  return {
+    status: -1,
+    title,
+    detail,
+    meta: {
+      cause: cause ? cause : null,
+      details: details ? details : null,
+    },
+  }
 }
