10bb8d514e490148cd07544e4e22958b1211f8d8
Chance Snow <git@chancesnow.me>
2017-07-02 15:17:37 -0700

    Export errors constructor via api module

--- ts/api.ts
+++ ts/api.ts
@@ -1,7 +1,10 @@
+import * as errors from './api/request/errors'
+
 export { setPartyApiHost, get, post } from './api/request'
 export {
   Response, ResponsePromise,
   Request, RequestParam,
 } from './api/request/primitives'
+export const Errors = errors.default
 export { Party } from './api/party'
 export { Track, TrackArtist, Image, firstArtistName, largestImage } from './api/track'
